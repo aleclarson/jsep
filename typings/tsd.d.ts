56ffc8f52089134b028e20c1b412757f096e07fb
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2021-06-09 15:33:00 -0400

    fix(types): Expression type should be a union

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -1,113 +1,118 @@
 declare module 'jsep' {
-
 	namespace jsep {
-		export interface Expression {
-			type: ExpressionType;
+		export type Expression =
+			| ArrayExpression
+			| BinaryExpression
+			| CallExpression
+			| Compound
+			| ConditionalExpression
+			| Identifier
+			| Literal
+			| LogicalExpression
+			| MemberExpression
+			| ThisExpression
+			| UnaryExpression
+
+		export interface ArrayExpression {
+			type: 'ArrayExpression'
+			elements: Expression[]
 		}
 
-		export interface ArrayExpression extends Expression {
-			type: 'ArrayExpression';
-			elements: Expression[];
+		export interface BinaryExpression {
+			type: 'BinaryExpression'
+			operator: string
+			left: Expression
+			right: Expression
 		}
 
-		export interface BinaryExpression extends Expression {
-			type: 'BinaryExpression';
-			operator: string;
-			left: Expression;
-			right: Expression;
+		export interface CallExpression {
+			type: 'CallExpression'
+			arguments: Expression[]
+			callee: Expression
 		}
 
-		export interface CallExpression extends Expression {
-			type: 'CallExpression';
-			arguments: Expression[];
-			callee: Expression;
+		export interface Compound {
+			type: 'Compound'
+			body: Expression[]
 		}
 
-		export interface Compound extends Expression {
-			type: 'Compound';
-			body: Expression[];
+		export interface ConditionalExpression {
+			type: 'ConditionalExpression'
+			test: Expression
+			consequent: Expression
+			alternate: Expression
 		}
 
-		export interface ConditionalExpression extends Expression {
-			type: 'ConditionalExpression';
-			test: Expression;
-			consequent: Expression;
-			alternate: Expression;
+		export interface Identifier {
+			type: 'Identifier'
+			name: string
 		}
 
-		export interface Identifier extends Expression {
-			type: 'Identifier';
-			name: string;
+		export interface Literal {
+			type: 'Literal'
+			value: boolean | number | string
+			raw: string
 		}
 
-		export interface Literal extends Expression {
-			type: 'Literal';
-			value: boolean | number | string;
-			raw: string;
+		export interface LogicalExpression {
+			type: 'LogicalExpression'
+			operator: string
+			left: Expression
+			right: Expression
 		}
 
-		export interface LogicalExpression extends Expression {
-			type: 'LogicalExpression';
-			operator: string;
-			left: Expression;
-			right: Expression;
+		export interface MemberExpression {
+			type: 'MemberExpression'
+			computed: boolean
+			object: Expression
+			property: Expression
 		}
 
-		export interface MemberExpression extends Expression {
-			type: 'MemberExpression';
-			computed: boolean;
-			object: Expression;
-			property: Expression;
+		export interface ThisExpression {
+			type: 'ThisExpression'
 		}
 
-		export interface ThisExpression extends Expression {
-			type: 'ThisExpression';
+		export interface UnaryExpression {
+			type: 'UnaryExpression'
+			operator: string
+			argument: Expression
+			prefix: boolean
 		}
 
-		export interface UnaryExpression extends Expression {
-			type: 'UnaryExpression';
-			operator: string;
-			argument: Expression;
-			prefix: boolean;
-		}
+		type ExpressionType = Expression['type']
 
-		type ExpressionType =
-			'Compound'
-			| 'Identifier'
-			| 'MemberExpression'
-			| 'Literal'
-			| 'ThisExpression'
-			| 'CallExpression'
-			| 'UnaryExpression'
-			| 'BinaryExpression'
-			| 'LogicalExpression'
-			| 'ConditionalExpression'
-			| 'ArrayExpression';
+		type HookType =
+			| 'gobble-expression'
+			| 'after-expression'
+			| 'gobble-token'
+			| 'after-token'
+			| 'gobble-spaces'
+		type HookCallback = (env: { node?: Expression }) => void
 
-		type HookType = 'gobble-expression' | 'after-expression' | 'gobble-token' | 'after-token' | 'gobble-spaces';
-		type HookCallback = (env: { node?: Expression }) => void;
+		function hooksAdd(name: HookType, cb: HookCallback): void
+		function hooksAdd(name: HookType, first: boolean, cb: HookCallback): void
+		function hooksAdd(obj: { [name in HookType]: HookCallback }): void
+		function hooksAdd(
+			obj: { [name in HookType]: HookCallback },
+			first: boolean
+		): void
 
-		function hooksAdd(name: HookType, cb: HookCallback): void;
-		function hooksAdd(name: HookType, first: boolean, cb: HookCallback): void;
-		function hooksAdd(obj: { [name in HookType]: HookCallback }): void;
-		function hooksAdd(obj: { [name in HookType]: HookCallback }, first: boolean): void;
+		function addBinaryOp(operatorName: string, precedence: number): void
 
-		function addBinaryOp(operatorName: string, precedence: number): void;
+		function addUnaryOp(operatorName: string): void
 
-		function addUnaryOp(operatorName: string): void;
+		function removeBinaryOp(operatorName: string): void
 
-		function removeBinaryOp(operatorName: string): void;
+		function removeUnaryOp(operatorName: string): void
 
-		function removeUnaryOp(operatorName: string): void;
+		function addIdentifierChar(identifierName: string): void
 
-		function addIdentifierChar(identifierName: string): void;
+		function removeIdentifierChar(identifierName: string): void
 
-		function removeIdentifierChar(identifierName: string): void;
-
-		const version: string;
+		const version: string
 	}
 
-	function jsep(val: string | jsep.Expression): jsep.Expression;
+	function jsep(val: string | jsep.Expression): jsep.Expression
 
-	export = jsep;
+	export = jsep
 }
